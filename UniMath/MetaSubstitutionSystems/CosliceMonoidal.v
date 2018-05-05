0307ff551d2c3806154914d0d6be3998935fc220
varkor <github@varkor.com>
2018-05-05 16:11:07 +0100

    Add coslice category

--- UniMath/MetaSubstitutionSystems/CosliceMonoidal.v
+++ UniMath/MetaSubstitutionSystems/CosliceMonoidal.v
@@ -6,6 +6,94 @@ Require Import UniMath.CategoryTheory.UnderCategories. (* for UnderCategory *)
 
 Local Open Scope cat.
 
+Section Coslice_Precat_Definition.
+
+Context (C : precategory) (A : C).
+
+Definition coslice_ob : UU := ∑ X : C, A --> X.
+
+Notation "⌑ S" := (pr1 S) (at level 26).
+Notation "↓ S" := (pr2 S) (at level 26).
+
+Definition is_coslice_mor (S S' : coslice_ob) (f : ⌑S --> ⌑S') : UU :=
+  ↓S · f = ↓S'.
+
+Definition coslice_mor (S S' : coslice_ob) : UU :=
+  ∑ f : ⌑S --> ⌑S', is_coslice_mor S S' f.
+
+Notation "⊸ f" := (pr1 f) (at level 26).
+Notation "△ f" := (pr2 f) (at level 26).
+
+Definition isaset_coslice_mor (hs : has_homsets C) (S S' : coslice_ob) : isaset (coslice_mor S S').
+Proof.
+  apply (isofhleveltotal2 2).
+  - apply hs.
+  - intro f.
+    apply isasetaprop.
+    apply hs.
+Qed.
+
+Definition coslice_mor_eq (hs : has_homsets C) (S S' : coslice_ob) (f g : coslice_mor S S') : ⊸ f = ⊸ g ≃ f = g.
+Proof.
+  apply invweq.
+  apply subtypeInjectivity.
+  intro a.
+  apply hs.
+Defined.
+
+Definition coslice_mor_id (S : coslice_ob) : coslice_mor S S.
+Proof.
+  exists (identity _ ).
+  unfold is_coslice_mor.
+  rewrite id_right.
+  apply idpath.
+Defined.
+
+Definition coslice_mor_comp (S S' S'' : coslice_ob) (f : coslice_mor S S') (g : coslice_mor S' S'') : coslice_mor S S''.
+Proof.
+  exists (⊸ f · ⊸ g).
+  unfold is_coslice_mor.
+  rewrite assoc.
+  rewrite (△ f).
+  rewrite (△ g).
+  apply idpath.
+Defined.
+
+Definition precategory_coslice_ob_mor : precategory_ob_mor.
+Proof.
+  exists coslice_ob.
+  exact coslice_mor.
+Defined.
+
+Definition precategory_coslice_data : precategory_data.
+Proof.
+  exists precategory_coslice_ob_mor.
+  exists coslice_mor_id.
+  exact coslice_mor_comp.
+Defined.
+
+Lemma is_precategory_precategory_coslice_data (hs : has_homsets C)
+  : is_precategory precategory_coslice_data.
+Proof.
+  repeat split; intros; simpl.
+  - apply coslice_mor_eq.
+    + apply hs.
+    + apply id_left.
+  - apply coslice_mor_eq.
+    + apply hs.
+    + apply id_right.
+  - apply coslice_mor_eq.
+    + apply hs.
+    + apply assoc.
+Qed.
+
+Definition precategory_Coslice (hs : has_homsets C)
+  : precategory := tpair _ _ (is_precategory_precategory_coslice_data hs).
+
+Local Notation Coslice := precategory_Coslice.
+
+End Coslice_Precat_Definition.
+
 Context (Mon : monoidal_precat).
 
 Let V := monoidal_precat_to_precat Mon.
@@ -15,6 +103,8 @@ Notation "f #⊗ g" := (#(monoidal_precat_to_tensor Mon) (f #, g)) (at level 31)
 Let α := monoidal_precat_to_associator Mon.
 Let λ' := monoidal_precat_to_left_unitor Mon.
 Let ρ := monoidal_precat_to_right_unitor Mon.
+
+Definition tensor := monoidal_precat_to_tensor Mon.
 
 Context (hs_V : has_homsets V).
 
