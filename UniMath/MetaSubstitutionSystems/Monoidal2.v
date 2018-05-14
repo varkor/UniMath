6e1d729b00cd019bebdcce7b59994bd99f2ec5ea
varkor <github@varkor.com>
2018-05-14 11:44:12 +0100

    Define natural isomorphism

--- UniMath/MetaSubstitutionSystems/Monoidal2.v
+++ UniMath/MetaSubstitutionSystems/Monoidal2.v
@@ -60,24 +60,68 @@ Defined.
 
 Definition iso {C: precategory_data}(a b : C) := total2 (fun f : a --> b => is_iso f).
 
-Definition mk_iso {C : precategory} {c c' : C} (f : c --> c') (ii : is_iso f) : iso c c'.
+Definition mk_iso {C : precategory} {c c' : C} {f : c --> c'} (ii : is_iso f) : iso c c'.
 Proof.
   exists f.
   exact ii.
 Defined.
 
-Definition is_nat_iso {C D : precategory} {F G : C ⟶ D} (μ : F ⟹ G) (ν : G ⟹ F) : UU :=
-  ∏ (c : C), ∑ (ii : is_iso (μ c)), (ν c = inv_from_iso (mk_iso (μ c) ii)).
+Definition is_nat_iso {C D : precategory} {F G : C ⟶ D} (μ : F ⟹ G) : UU :=
+  ∏ (c : C), is_iso (μ c).
+
+Definition is_nat_id {C D : precategory} {F : C ⟶ D} (μ : F ⟹ F) : UU :=
+  ∏ (c : C), μ c = id (F c).
 
 Definition nat_iso {C D : precategory} (F G : C ⟶ D) : UU
-  := ∑ (μ : F ⟹ G), ∑ (ν : G ⟹ F), is_nat_iso μ ν.
+  := ∑ (μ : F ⟹ G), is_nat_iso μ.
+
+Definition iso_inv_after_iso' {C : precategory} {a b : C} (f : a --> b) (f' : iso a b) (deref : pr1 f' = f) : f · inv_from_iso f' = identity _.
+Proof.
+  rewrite <- deref.
+  exact (iso_inv_after_iso f').
+Defined.
+
+Definition iso_after_iso_inv' {C : precategory} {a b : C} (f : a --> b) (f' : iso a b) (deref : pr1 f' = f) : inv_from_iso f' · f = identity _.
+Proof.
+  rewrite <- deref.
+  exact (iso_after_iso_inv f').
+Defined.
+
+Definition nat_iso_inv {C D : precategory} {F G : C ⟶ D} (μ : nat_iso F G) : nat_iso G F.
+Proof.
+  pose (iso := (λ c, mk_iso (pr2 μ c))).
+  pose (inv := (λ c, inv_from_iso (iso c))).
+  use tpair.
+  - exists inv.
+    intros c c' f.
+    pose (coher := pr2 (pr1 μ) c c' f).
+    pose (coher_inv := maponpaths (λ p, inv c · p · inv c') coher).
+    simpl in coher_inv.
+    repeat rewrite <- assoc in coher_inv.
+    unfold inv in coher_inv.
+    assert (deref' : pr1 (iso c') = (pr1 μ) c') by reflexivity.
+    force (coher_inv : (inv_from_iso (iso c) · (# F f · ((pr1 μ) c' · inv_from_iso (iso c'))) = inv_from_iso (iso c) · (pr1 (pr1 μ) c · (# G f · inv_from_iso (iso c'))))).
+    rewrite (iso_inv_after_iso' ((pr1 μ) c') (iso c') deref') in coher_inv.
+    rewrite id_right in coher_inv.
+    repeat rewrite assoc in coher_inv.
+    assert (deref : pr1 (iso c) = (pr1 μ) c) by reflexivity.
+    force (coher_inv : (inv_from_iso (iso c) · # F f = inv_from_iso (iso c) · (pr1 μ) c · # G f · inv_from_iso (iso c'))).
+    rewrite (iso_after_iso_inv' ((pr1 μ) c) (iso c) deref) in coher_inv.
+    rewrite id_left in coher_inv.
+    unfold inv.
+    symmetry.
+    assumption.
+  - intro c.
+    exact (is_iso_inv_from_iso (iso c)).
+Defined.
+
 
 Definition nat_iso_to_trans {C D : precategory} {F G : C ⟶ D} (ν : nat_iso F G) : F ⟹ G :=
   pr1 ν.
 
 (* ⁻¹ *)
 Definition nat_iso_to_trans_inv {C D : precategory} {F G : C ⟶ D} (ν : nat_iso F G) : G ⟹ F :=
-pr1 (pr2 ν).
+  pr1 (nat_iso_inv ν).
 
 Definition is_nat_iso_id {C D : precategory} {F G : C ⟶ D} (ν : nat_iso F G) : UU :=
   ∏ (c : C), (nat_iso_to_trans ν c) · (nat_iso_to_trans_inv ν c) = id (F c).
@@ -223,16 +267,17 @@ Definition triangle_eq (λ' : left_unitor) (ρ' : right_unitor) (α' : associato
 Definition pentagon_eq (α' : associator) : UU :=
   ∏ (a b c d : C), pr1 α' ((a ⊗ b, c), d) · pr1 α' ((a, b), c ⊗ d) = pr1 α' ((a, b), c) #⊗ id d · pr1 α' ((a, b ⊗ c), d) · id a #⊗ pr1 α' ((b, c), d).
 
-Definition monoidal_struct : UU :=
-  ∑ (λ' : left_unitor), ∑ (ρ' : right_unitor), ∑ (α' : associator), (triangle_eq λ' ρ' α') × (pentagon_eq α').
-
 Definition is_strict (λ' : left_unitor) (ρ' : right_unitor) (α' : associator) : UU :=
   (is_nat_iso_id λ') × (is_nat_iso_id ρ') × (is_nat_iso_id α').
 
 End Monoidal_Precat.
 
 Definition monoidal_precat : UU :=
-  ∑ C : precategory, ∑ tensor : C ⊠ C ⟶ C, ∑ I : C, monoidal_struct C tensor I.
+  ∑ C : precategory, ∑ tensor : C ⊠ C ⟶ C, ∑ I : C,
+  ∑ λ' : left_unitor C tensor I,
+  ∑ ρ' : right_unitor C tensor I,
+  ∑ α' : associator C tensor,
+  (triangle_eq C tensor I λ' ρ' α') × (pentagon_eq C tensor α').
 
 Section Monoidal_Precat_Accessors.
 
