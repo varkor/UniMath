33fbfbc5879d9a1512753a6cf124a228de5c1705
varkor <github@varkor.com>
2018-05-14 11:43:42 +0100

    Define Strong_Monoidal_Functor_Action

--- UniMath/MetaSubstitutionSystems/ActionsAndStrengths.v
+++ UniMath/MetaSubstitutionSystems/ActionsAndStrengths.v
@@ -3,21 +3,21 @@ Require Import UniMath.CategoryTheory.Categories. (* for precategory *)
 Require Import UniMath.CategoryTheory.functor_categories. (* for functor *)
 Require Import UniMath.MetaSubstitutionSystems.Monoidal2. (* for binprod_precat *)
 Require Import UniMath.CategoryTheory.limits.binproducts. (* for BinProducts *)
-Require Import UniMath.MetaSubstitutionSystems.CosliceMonoidal. (* for precategory_Coslice *)
+Require Import UniMath.MetaSubstitutionSystems.CosliceMonoidal. (* for precategory_coslice *)
 Require Import UniMath.MetaSubstitutionSystems.ForceTactic. (* for force_goal *)
 
 Local Open Scope cat.
 
-Context (Mon : monoidal_precat).
+Context (Mon_V : monoidal_precat).
 
-Let V := monoidal_precat_precat Mon.
-Let I := monoidal_precat_unit Mon.
-Let tensor := monoidal_precat_tensor Mon.
+Let V := monoidal_precat_precat Mon_V.
+Let I := monoidal_precat_unit Mon_V.
+Let tensor := monoidal_precat_tensor Mon_V.
 Notation "X ⊗ Y" := (tensor (X , Y)) (at level 31).
 Notation "f #⊗ g" := (#tensor (f #, g)) (at level 31).
-Let α' := monoidal_precat_associator Mon.
-Let λ' := monoidal_precat_left_unitor Mon.
-Let ρ' := monoidal_precat_right_unitor Mon.
+Let α' := monoidal_precat_associator Mon_V.
+Let λ' := monoidal_precat_left_unitor Mon_V.
+Let ρ' := monoidal_precat_right_unitor Mon_V.
 
 Section Actions_Definition.
 
@@ -117,7 +117,7 @@ End Actions_Natural_Transformations.
 
 (* Action *)
 
-Definition action : UU := ∑ A : precategory, ∑ (odot : functor (binprod_precat A V) A), ∑ (ϱ : action_right_unitor A odot), ∑ (χ : action_convertor A odot), (action_triangle_eq A odot ϱ χ) × (action_pentagon_eq A odot χ).
+Definition action : UU := ∑ A : precategory, ∑ (odot : A ⊠ V ⟶ A), ∑ (ϱ : action_right_unitor A odot), ∑ (χ : action_convertor A odot), (action_triangle_eq A odot ϱ χ) × (action_pentagon_eq A odot χ).
 
 End Actions_Definition.
 
@@ -213,7 +213,7 @@ Proof.
   exists tensor.
   exists ρ'.
   exists α'.
-  exact (monoidal_precat_eq Mon).
+  exact (monoidal_precat_eq Mon_V).
 Defined.
 
 (* F(A) ⊗ B --> F(A ⊗ B) *)
@@ -231,3 +231,84 @@ Admitted.
 Definition cartesian_strength := strength cartesian_action cartesian_action.
 
 End Homogeneous_Strengths.
+
+Section Strong_Monoidal_Functor_Action.
+
+Context (Mon_A : monoidal_precat).
+
+Let A := monoidal_precat_precat Mon_A.
+Let I_A := monoidal_precat_unit Mon_A.
+Let tensor_A := monoidal_precat_tensor Mon_A.
+Notation "X ⊗_A Y" := (tensor_A (X , Y)) (at level 31).
+Notation "f #⊗_A g" := (#tensor_A (f #, g)) (at level 31).
+Let α_A := monoidal_precat_associator Mon_A.
+Let λ_A := monoidal_precat_left_unitor Mon_A.
+Let ρ_A := monoidal_precat_right_unitor Mon_A.
+
+Context (U : strong_monoidal_functor Mon_V Mon_A).
+
+Definition otimes_U_functor : A ⊠ V ⟶ A.
+use tpair.
+- use tpair.
+  exact (λ av, av true ⊗_A U (av false)).
+  intros ? ? f.
+  exact (f true #⊗_A #U (f false)).
+- split.
+  + intro.
+    simpl.
+    rewrite (functor_id U).
+    rewrite (binprod_proj_id a).
+    rewrite binprod_id.
+    rewrite (functor_id tensor_A).
+    reflexivity.
+  + unfold functor_compax.
+    simpl.
+    intros.
+    do 2 rewrite (binprod_proj_comp f).
+    rewrite (functor_comp U).
+    rewrite binprod_comp.
+    rewrite (functor_comp tensor_A).
+    reflexivity.
+Defined.
+
+Definition U_action : action.
+Proof.
+  exists A.
+  exists otimes_U_functor.
+  (* K ⊗ U I_C -- (1_K ⊗ ϵ^{-1} · λ_D K) --> K *)
+  use tpair.
+  - (* ρ *)
+  unfold action_right_unitor.
+  unfold nat_iso.
+  use tpair.
+  unfold nat_trans.
+  pose (ϵ := pr1 (pr2 (pr1 U))).
+  pose (ϵ_inv := inv_from_iso (mk_iso ϵ (pr1 (pr2 U)))).
+  use tpair.
+  intro.
+  exact (id x #⊗_A ϵ_inv · pr1 ρ_A x). (* ϱ *)
+  intros x x' f.
+  pose (ρ_nat_law := pr2 (pr1 ρ_A) x x' f).
+  simpl; simpl in ρ_nat_law.
+  assert (ϵ_coher : id x #⊗_A ϵ_inv · f #⊗_A id I_A = f #⊗_A (#U (id I)) · id x' #⊗_A ϵ_inv).
+  - do 2 rewrite <- functor_comp.
+    do 2 rewrite <- binprod_comp.
+    rewrite functor_id.
+    do 2 (rewrite id_left; rewrite id_right).
+    reflexivity.
+  - rewrite assoc.
+    force (ϵ_coher : (# tensor_A (id x #, ϵ_inv) · # tensor_A (f #, id I_A) = # tensor_A (f #, # U (id I)) · # tensor_A (id x' #, ϵ_inv))).
+    force_goal (# tensor_A (f #, # U (id I)) · # tensor_A (id x' #, ϵ_inv) · (pr1 ρ_A) x' = # tensor_A (id x #, ϵ_inv) · (pr1 ρ_A) x · f).
+   rewrite <- ϵ_coher.
+   repeat rewrite <- assoc.
+   force_goal (# tensor_A (id x #, ϵ_inv) · (# tensor_A (f #, id I_A) · (pr1 ρ_A) x') = # tensor_A (id x #, ϵ_inv) · (pr1 (pr1 ρ_A) x · f)).
+   force (ρ_nat_law : (# (pr1 (pr2 Mon_A)) (f #, id pr1 (pr2 (pr2 Mon_A))) · pr1 (pr1 ρ_A) x' = pr1 (pr1 ρ_A) x · f)).
+    rewrite <- ρ_nat_law.
+    reflexivity.
+  - intro.
+  exists ρ'.
+  exists α'.
+  exact (monoidal_precat_eq Mon_V).
+Defined.
+
+End Strong_Monoidal_Functor_Action.
