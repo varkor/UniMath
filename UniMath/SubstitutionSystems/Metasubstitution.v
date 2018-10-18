21d77a12bc1b63ad115e214722552e161833953a
varkor <github@varkor.com>
2018-10-18 11:24:59 +0100

    Define substitution in terms of a monad multiplication

--- UniMath/SubstitutionSystems/Metasubstitution.v
+++ UniMath/SubstitutionSystems/Metasubstitution.v
@@ -4,6 +4,8 @@ Require Import UniMath.CategoryTheory.functor_categories.
 Require Import UniMath.CategoryTheory.Monads.Monads.
 Require Import UniMath.CategoryTheory.limits.binproducts.
 Require Import UniMath.CategoryTheory.limits.bincoproducts.
+Require Import UniMath.CategoryTheory.Monoidal.Actions.
+Require Import UniMath.CategoryTheory.Monoidal.Strengths.
 
 Local Open Scope cat.
 
@@ -42,3 +44,33 @@ Definition exp_map {X Y : C} (X_Y : exponential_ob X Y) : exp_ob X_Y ⨰ Y --> X
 Definition exp_map' {X Y : C} (X_Y : exponential_ob X Y) : Y ⨰ exp_ob X_Y --> X := (BinProductSwap _ (bin_prod Y (exp_ob X_Y)) (bin_prod (exp_ob X_Y) Y)) · (exp_map X_Y).
 
 End Exponentials.
+
+Context (exp : exponentials).
+
+Local Notation "X ^ Y" := (exp_ob (exp X Y)).
+
+Context (n : C).
+
+Check n ∔ n.
+
+Context (T : Monad C).
+
+(* FIXME: use `strength` in the definition *)
+Definition cartesian_strength (F : C ⟶ C) : UU := ∏ (A B : C), (F A) ⨰ B --> F (A ⨰ B).
+Context (st : cartesian_strength T).
+(* FIXME: define distributivity in terms of a `strength` *)
+Definition distributivity : UU := ∏ (X Y Z : C), (X ∔ Y) ⨰ Z --> X ⨰ Z ∔ Y ⨰ Z.
+Context (dist : distributivity).
+
+Definition substitution_from_monad (S : C) : (T (S ∔ n)) ⨰ ((T S) ^ n) --> T S.
+Proof.
+  exact (
+    (* T(S + n) × (TS)^n *)
+    (st (S ∔ n) (T S ^ n)) (* T((S + n) × (TS)^n) *)
+    · #T (dist S n (T S ^ n)) (* T(S × (TS)^n + n × (TS)^n) *)
+    · #T ((BinProductPr1 _ (bin_prod S (T S ^ n))) #∔ (exp_map' (exp (T S) n))) (* T(S + TS) *)
+    · #T (η T S #∔ id (T S)) (* T(TS + TS)*)
+    · #T (BinCoproductArrow _ (bin_coprod (T S) (T S)) (id (T S)) (id (T S))) (* T(TS) *)
+    · (μ T S) (* TS *)
+  ).
+Defined.
